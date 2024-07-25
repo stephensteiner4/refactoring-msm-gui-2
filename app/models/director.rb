c31c8a4955362fab821654d184ff97db6dbb56f2
Stephen Steiner <46692873+stephensteiner4@users.noreply.github.com>
2024-07-25 02:29:59 +0000

    finish refactor assignment

--- app/models/director.rb
+++ app/models/director.rb
@@ -13,11 +13,12 @@
 class Director < ApplicationRecord
   validates(:name, presence: true)
 
-  def filmography
-    key = self.id
+  has_many(:filmography, class_name: "Movie", foreign_key: "director_id")
+  # def filmography
+  #   key = self.id
 
-    the_many = Movie.where({ :director_id => key })
+  #   the_many = Movie.where({ :director_id => key })
 
-    return the_many
-  end
+  #   return the_many
+  # end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -16,13 +16,17 @@ class Movie < ApplicationRecord
   validates(:director_id, presence: true)
   validates(:title, uniqueness: true)
 
-  def director
-    key = self.director_id
+  belongs_to(:director)
 
-    matching_set = Director.where({ :id => key })
+  has_many(:characters)
+  has_many(:actors, through: :characters, source: :actor)
+  # def director
+  #   key = self.director_id
 
-    the_one = matching_set.at(0)
+  #   matching_set = Director.where({ :id => key })
 
-    return the_one
-  end
+  #   the_one = matching_set.at(0)
+
+  #   return the_one
+  # end
 end
